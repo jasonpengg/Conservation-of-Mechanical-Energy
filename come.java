9901cebe22b44d4fa0d31363ffe9b40d9ca0a0bf
samchan17 <130682583+samchan17@users.noreply.github.com>
2023-11-20 22:07:01 -0500

    Update come.java

--- come.java
+++ come.java
@@ -1,18 +1,39 @@
 // Sam and Jason
 // Version 1.0
 // Conservation of Mechanical Energy
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import java.io.*;
 
 public class come{
 	// Properties
+	JFrame theframe = new JFrame("Conservation of Mechanical Energy");
+	JPanel thepanel = new JPanel();
+	JTextField thetext = new JTextField();
+	JButton runbutton = new JButton();
+	JButton resetbutton = new JButton();
+	JLabel vlabel = new JLabel();
+	JLabel hlabel = new JLabel();
+	JLabel maxhlabel = new JLabel();
+	
 	
 	
 	
 	// Methods
 	
 	
-	// Constructor
-	
+	// Constructor: shm is simple harmonic motion
+	public shm(){
+		thepanel.setLayout(null);
+		thepanel.setPreferredSize(new Dimension(960, 540));
+		
+		
+	}
 	
 	// Main Method
-
+	public static void main(String[] args){
+		new shm();
+	}
 }
